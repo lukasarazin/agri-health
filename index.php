52306e8325b0f195f88682ac41281c4fcf66b5cb
Oscar DAVASE <oscardavase@gmail.com>
2022-10-06 15:15:40 +0200

    sedghj

--- index.php
+++ index.php
@@ -90,27 +90,7 @@ require_once 'functions/test.php';
             </div>
         </section>
 
-        <section id="stats" style="padding: 50px;">
-            <div class="container">
-                <div class="text-center row">
-                    <div class="col-6" style="margin-left: 115px;">
-                            <h3 class="stat" style="font-size: 25px; text-align: left; margin-bottom: 20px;"><span style="padding-bottom: 200px;">La tech au service de la vie</span></h3>
-                            <p style="font-size: 15px; font-weight: 400; text-align: left; margin-bottom: 30px;">Comprenez mieux vos animaux et <br>aidez les à aller mieux en même temps.</p>
-                        <div class="row" >
-                            <img class="col-4" src="http://localhost/agri-health/assets/images/insta.png" style="
-                            width: 61px; height: 37px;">
-                            <img class="col-4" src="http://localhost/agri-health/assets/images/facebook.png" style="
-                            width: 60px; height: 35px;">
-                            <img class="col-4" src="http://localhost/agri-health/assets/images/lk.png" style="
-                            width: 60px; height: 35px;">
-                        </div>  
-                    </div>
-                    <div class="col-6">
-                         
-                    </div>
-                </div>
-            </div>
-        </section>
+
     </main>
 
 <?php require_once 'template-parts/layout/footer.php'; ?>
