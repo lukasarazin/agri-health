2bba9fca57806c87f684f6dfe9e8ed8df5af06d7
Oscar DAVASE <oscardavase@gmail.com>
2022-10-06 15:25:09 +0200

    update :zetyuio

--- functions/database.php
+++ functions/database.php
@@ -3,7 +3,7 @@
 function connectDB()
 {
     try {
-        return new PDO('mysql:host=localhost;dbname=agrihealth;charset=UTF8', 'root', '');
+        return new PDO('mysql:host=localhost;dbname=agrihealth;charset=UTF8', 'root', 'wordpress');
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage() . "<br/>";
         die();
